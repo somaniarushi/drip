e927d6229786183bb3f99566e25e335e7620c118
Arushi Somani <arushisomani@berkeley.edu>
2023-02-18 11:02:40 -0800

    aws stuff no cors work

--- api/app.py
+++ api/app.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 api = Flask(__name__)
 cors = CORS(api)
@@ -12,3 +17,22 @@ def hello():
     }
 
     return response_body
+
+@api.route('/roast')
+def roast():
+    image = request.args.get('desc')
+    # Make a call to openAI API
+    res = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Write a roast of a dress in the style of Miranda Priestly from The Devil Wears Prada.",
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+
+    # print(res)
+    return {
+        "description": res.choices[0].text
+    }
+
--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import S3FileUpload from 'react-s3';
+import { uploadFile } from "react-s3"
+window.Buffer = window.Buffer || require("buffer").Buffer;
+
+
+const config = {
+  bucketName: 'dripdownbucket',
+  region: 'us-west-2',
+  accessKeyId: 'ACCESS_KEY_ID',
+  secretAccessKey: 'SECRET_ACCESS_KEY',
+  s3Url: 'https://dripdownbucket.s3.amazonaws.com/'
+}
 
 const FLASK_APP = "http://127.0.0.1:5000";
 
@@ -17,15 +29,35 @@ function App() {
     setImage(e.target.files[0]);
   }
 
-  function getResponse() {
+  async function getResponse() {
+    const res = await uploadFile(image, config)
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
+
+    console.log(res);
+
     axios({
       method: 'GET',
-      url: `${FLASK_APP}/hello`, // change this to your flask server
-    }).then((res) => {
-      setResponse(res.data);
+      url: `dripdownbucket.s3.amazonaws.com/me.jpeg`, // change this to your flask server
+    })
+    .then((res) => {
+      console.log(res);
     }).catch((err) => {
       console.log(err);
-    });
+    })
+
+
+    // axios({
+    //   method: 'GET',
+    //   url: `${FLASK_APP}/roast`, // change this to your flask server
+    //   params: {
+    //     image: imageURL
+    //   }
+    // }).then((res) => {
+    //   setResponse(res.data);
+    // }).catch((err) => {
+    //   console.log(err);
+    // });
   }
 
   return (
@@ -41,7 +73,7 @@ function App() {
           <p className="text-center">
             {response['description'].toString()}
           </p>
-        }   
+        }
       </div>
     </div>
   );
