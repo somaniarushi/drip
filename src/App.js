5794bce584e7b1d51b4f8528ed288332b6e86700
cnnmon <cnnmonn@gmail.com>
2023-02-18 14:18:42 -0800

    add .env

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import { useState, useEffect } from 'react';
-import { uploadFile } from "react-s3"
+import React, { useState, useEffect } from 'react';
+import { uploadFile } from "react-s3";
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
-const BUCKET = "dripdrownbucket"
-const ACCESS_KEY = "AKIAQVO2Z7MRUIAATZTG"
-const SECRET_KEY = "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"
-
 const config = {
-  bucketName: `${BUCKET}`,
+  bucketName: `${process.env.REACT_APP_BUCKET}`,
   region: 'us-west-2',
-  accessKeyId: ACCESS_KEY,
-  secretAccessKey: SECRET_KEY,
-  s3Url: `https://${BUCKET}.s3.amazonaws.com/`
+  accessKeyId: process.env.REACT_APP_ACCESS_KEY,
+  secretAccessKey: process.env.REACT_APP_SECRET_KEY,
+  s3Url: `https://${process.env.REACT_APP_BUCKET}.s3.amazonaws.com/`
 }
 
 const FLASK_APP = "http://127.0.0.1:5000";
@@ -19,7 +15,7 @@ const FLASK_APP = "http://127.0.0.1:5000";
 function App() {
   const [image, setImage] = useState(null);
   const [imageURL, setImageURL] = useState("");
-
+  
   useEffect(() => {
     if (image == null) return;
     setImageURL(URL.createObjectURL(image));
@@ -46,7 +42,7 @@ function App() {
         {imageURL ? <img src={imageURL} alt="preview upload" className="w-64 h-64" /> : <input type="file" onChange={onImageChange} />}
       </div>
       <div>
-        <button onClick={uploadImage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button onClick={() => {}} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Upload Image
         </button>
       </div>
