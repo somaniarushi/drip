51c1e7a3aad5d2845c2c1a500d5b7a9d1340f43a
cnnmon <cnnmonn@gmail.com>
2023-02-18 14:05:37 -0800

    clean

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
-import S3FileUpload from 'react-s3';
 import { uploadFile } from "react-s3"
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
+const BUCKET = "dripdrownbucket"
+const ACCESS_KEY = "AKIAQVO2Z7MRUIAATZTG"
+const SECRET_KEY = "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"
 
 const config = {
-  bucketName: 'dripdrownbucket',
+  bucketName: `${BUCKET}`,
   region: 'us-west-2',
-  accessKeyId: 'ACCESS_KEY_ID',
-  secretAccessKey: 'SECRET_ACCESS_KEY',
-  s3Url: 'https://dripdrownbucket.s3.amazonaws.com/'
+  accessKeyId: ACCESS_KEY,
+  secretAccessKey: SECRET_KEY,
+  s3Url: `https://${BUCKET}.s3.amazonaws.com/`
 }
 
 const FLASK_APP = "http://127.0.0.1:5000";
 
 function App() {
-  const [response, setResponse] = useState(null);
   const [image, setImage] = useState(null);
   const [imageURL, setImageURL] = useState("");
 
@@ -29,42 +29,15 @@ function App() {
     setImage(e.target.files[0]);
   }
 
-  async function getResponse() {
-    const res = await uploadFile(image, config)
-    .then(data => console.log(data))
+  async function uploadImage() {
+    if (image == null) {
+      console.log("No image!");
+      return;
+    }
+
+    await uploadFile(image, config)
+    .then(data => setImageURL(data.location))
     .catch(err => console.error(err));
-
-    console.log(res);
-    // let config2 = {
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     'Access-Control-Allow-Origin': '*',
-    //     }
-    // }
-
-    // axios({
-    //   method: 'GET',
-    //   url: `https://dripdrownbucket.s3.amazonaws.com/me.jpeg`, // change this to your flask server,
-    //   config2
-    // })
-    // .then((res) => {
-    //   console.log(res);
-    // }).catch((err) => {
-    //   console.log(err);
-    // })
-
-
-    // axios({
-    //   method: 'GET',
-    //   url: `${FLASK_APP}/roast`, // change this to your flask server
-    //   params: {
-    //     image: imageURL
-    //   }
-    // }).then((res) => {
-    //   setResponse(res.data);
-    // }).catch((err) => {
-    //   console.log(err);
-    // });
   }
 
   return (
@@ -73,14 +46,9 @@ function App() {
         {imageURL ? <img src={imageURL} alt="preview upload" className="w-64 h-64" /> : <input type="file" onChange={onImageChange} />}
       </div>
       <div>
-        <button onClick={getResponse} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Get Response
+        <button onClick={uploadImage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Upload Image
         </button>
-        {response &&
-          <p className="text-center">
-            {response['description'].toString()}
-          </p>
-        }
       </div>
     </div>
   );
