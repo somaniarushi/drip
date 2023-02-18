7e00e60704621732fa948d88c1f35063a287b6a9
Austin George <ageorge8000@gmail.com>
2023-02-18 11:18:56 -0800

    Refactor Vision Transformer for cleanliness

--- api/app.py
+++ api/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from transformers import ViltProcessor, ViltForQuestionAnswering
-import requests
-from PIL import Image
+
+from VisionTransformer import *
 
 api = Flask(__name__)
 cors = CORS(api)
@@ -16,41 +15,15 @@ def hello():
 
     return response_body
 
-@api.route('/test')
-def test():
-    name = request.args['name']
-    res = "Hello " + name
-
-    response_body = {
-        "description": res,
-    }
-
-    return response_body
-
 @api.route('/describe')
 def describe():
+    """
+    Describes an image based on a given prompt.
+
+    @param: image: URL of the image
+    @param: prompt: question about the image (i.e. "What type of top is the person wearing?")
+    """
     url = request.args['image']
     prompt = request.args['prompt']
 
-    image = Image.open(requests.get(url, stream=True).raw)
-
-    processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
-    model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
-
-    # prepare inputs
-    encoding = processor(image, prompt, return_tensors="pt")
-
-    # forward pass
-    outputs = model(**encoding)
-    logits = outputs.logits
-
-    # Change logits to probabilities
-    probs = logits.softmax(-1)
-
-    # Get the top prediction
-    probs, idxs = probs.topk(1)
-
-    return jsonify({
-        "answer": model.config.id2label[idxs[0][0].item()],
-        "probability": probs[0].item()
-    })
+    return describe_image(url, prompt)
