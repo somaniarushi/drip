1d5a491166822865713ae6bb176a8f0c14b32b80
Arushi Somani <arushisomani@berkeley.edu>
2023-02-18 21:45:51 -0800

    progress bar

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { ProgressBar } from 'react-bootstrap';
+import '../index.css'
 
 function App() {
   const { state } = useLocation();
@@ -9,7 +11,34 @@ function App() {
       <h1>Results</h1>
       <p>Description: {state.description}</p>
       <p>Roast: {state.roast}</p>
-      <p>Rating: {state.rating}</p>
+      <Ratings ratings={state.rating} />
+    </div>
+  )
+}
+
+function Ratings({ ratings }) {
+  return (
+    <div>
+      <h1>Ratings</h1>
+      <p>Originality</p>
+      <div class="meter">
+          <span style={{width: ratings['originality'] + "0%"}}></span>
+      </div>
+
+      <p>Flair</p>
+      <div class="meter">
+          <span style={{width: ratings['flair'] + "0%"}}></span>
+      </div>
+
+      <p>Cohesiveness</p>
+      <div class="meter">
+          <span style={{width: ratings['cohesiveness'] + "0%"}}></span>
+      </div>
+
+      <p>Execution</p>
+      <div class="meter">
+          <span style={{width: ratings['execution'] + "0%"}}></span>
+      </div>
     </div>
   )
 }
