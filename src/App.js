6fd34bc5e48014e8f5b2b902fe2a26d8f3d263c9
cnnmon <cnnmonn@gmail.com>
2023-02-18 18:37:39 -0800

    loading

--- src/App.js
+++ src/App.js
@@ -16,16 +16,31 @@ const config = {
   s3Url: `https://${process.env.REACT_APP_BUCKET}.s3.amazonaws.com/`
 }
 const FLASK_APP = "http://127.0.0.1:5000";
+const LOADING_FLAVOR = [
+  "Analyzing your fit...",
+  "Evaluating your vibes...",
+  "Swiping right...",
+  "Constructing compliments...",
+  "Thinking of a witty caption...",
+  "Brainstorming your style...",
+]
 
 function App() {
   const [image, setImage] = useState(null);
   const [imageURL, setImageURL] = useState("");
   const [desc, setDesc] = useState("");
   const [loading, setLoading] = useState(false);
+  const [loadingFlavor, setLoadingFlavor] = useState(LOADING_FLAVOR[0]);
 
-  const [descUnclean, setDescUnclean] = useState("");
-  const [roast, setRoast] = useState("");
-  const [rating, setRating] = useState({});
+  // Rotating loading flavor text
+  useEffect(() => {
+    if (loading) {
+      const interval = setInterval(() => {
+        setLoadingFlavor(LOADING_FLAVOR[Math.floor(Math.random() * LOADING_FLAVOR.length)]);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  })
 
   useEffect(() => {
     if (image == null) return;
@@ -64,7 +79,6 @@ function App() {
   async function getRating(desc, roast) {
     const res = await axios.get(`${FLASK_APP}/rating?desc=${desc}&roast=${roast}`);
     console.log("Rating", res.data);
-    setRating(res.data.rating);
     return res.data.rating;
   }
 
@@ -72,14 +86,12 @@ function App() {
     const res = await axios.get(`${FLASK_APP}/desc?url=${imageURL}`);
     console.log("Data", res.data);
     setDesc(res.data.desc);
-    setDescUnclean(res.data.result);
     return res.data.desc;
   }
 
   async function getRoast() {
     axios.get(`${FLASK_APP}/roast?desc=${desc}`)
     .then(res => {
-      setRoast(res.data.critique);
       return res.data.critique;
     })
     .catch(err => console.error(err));
@@ -95,7 +107,7 @@ function App() {
           </h1>
         </div>
         <div className="text-1xl">
-          <p>Refine your aesthetic.<br />Curate excellence.</p>
+          <p>{loading ? loadingFlavor : "Refine your aesthetic.\nCurate excellence."}</p>
         </div>
       </div>
       <div className="flex items-center justify-center file-upload-container z-10">
@@ -103,7 +115,6 @@ function App() {
           (
             <div>
               <img src={imageURL} alt="preview upload" className="w-64 h-64" />
-              {loading && (<p>Loading...</p>)}
             </div>
           ) :
           <Upload submitImage={submitImage} />
@@ -112,9 +123,9 @@ function App() {
       {/* bg images */}
       <div className="z-0">
         <img src={person2} alt="decoration" className="absolute opacity-50 bg-image" style={{left: "35%", bottom: "-8%"}} />
-        <img src={person1} alt="decoration" className="absolute opacity-50 bg-image" style={{left: "45%", bottom: "10%"}} />
-        <img src={person3} alt="decoration" className="absolute opacity-50 bg-image" style={{left: "30%", top: "-10%"}} />
-        <img src={person4} alt="decoration" className="absolute opacity-50 bg-image" style={{left: "-5%", bottom: "10%"}} />
+        <img src={person1} alt="decoration" className="absolute opacity-20 bg-image" style={{left: "45%", bottom: "10%"}} />
+        <img src={person3} alt="decoration" className="absolute opacity-30 bg-image" style={{left: "30%", top: "-10%"}} />
+        <img src={person4} alt="decoration" className="absolute opacity-40 bg-image" style={{left: "-5%", bottom: "10%"}} />
       </div>
 
     </div>
